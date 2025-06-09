7898e8532571a0a51972939fc408c9287c13f48d
GabrielCam123 <cameotapia@gmail.com>
2025-06-09 09:40:28 -0400

    Sharktank3

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import { connectDB } from "./db.js";
 
 dotenv.config();
 connectDB();
-app.listen(4000);
-console.log("Server on port", 4000);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
